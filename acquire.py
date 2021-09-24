a9e30b114a7a757f76ea7bc9a245101024be8d11
Sarah Lawson Woods <Sarah.lawsonwoods@gmail.com>
2021-09-23 23:25:11 -0500

    Starting up zillow clustering project.

---
+++ acquire.py
@@ -0,0 +1,75 @@
+
+import pandas as pd
+import numpy as np
+import os
+from env import host, username, password
+
+###################### Acquire Zillow Data ######################
+
+url = f"mysql+pymysql://{username}:{password}@{host}/zillow"
+# this query brings in all the columns necessary to ask the questions posed by the zillow team  
+query = """ SELECT *
+    FROM properties_2017
+    LEFT OUTER JOIN airconditioningtype 
+    USING(airconditioningtypeid) 
+    LEFT OUTER JOIN architecturalstyletype
+    USING(architecturalstyletypeid) 
+    LEFT OUTER JOIN buildingclasstype 
+    USING(buildingclasstypeid) 
+    LEFT OUTER JOIN heatingorsystemtype
+    USING(heatingorsystemtypeid) 
+    LEFT OUTER JOIN predictions_2017
+    ON predictions_2017.id = properties_2017.id
+    INNER JOIN (
+    SELECT id, MAX(transactiondate) as last_trans_date 
+    FROM predictions_2017
+    GROUP BY id
+        ) predictions ON predictions.id = properties_2017.id AND predictions_2017.transactiondate             predictions.last_trans_date
+    LEFT OUTER JOIN propertylandusetype
+    USING(propertylandusetypeid)
+      LEFT OUTER JOIN storytype
+    USING(storytypeid)
+    LEFT OUTER JOIN typeconstructiontype
+    USING(typeconstructiontypeid)
+    JOIN unique_properties
+      ON unique_properties.parcelid = properties_2017.parcelid
+    WHERE latitude IS NOT NULL and longitude IS NOT NULL;"""
+
+def get_connection(db, username=username, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    It takes in a string name of a database as an argument.
+    '''
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+    
+def get_db():
+    df = pd.read_sql('''SELECT *
+    FROM properties_2017
+    LEFT OUTER JOIN airconditioningtype 
+    USING(airconditioningtypeid) 
+    LEFT OUTER JOIN architecturalstyletype
+    USING(architecturalstyletypeid) 
+    LEFT OUTER JOIN buildingclasstype 
+    USING(buildingclasstypeid) 
+    LEFT OUTER JOIN heatingorsystemtype
+    USING(heatingorsystemtypeid) 
+    LEFT OUTER JOIN predictions_2017
+    ON predictions_2017.id = properties_2017.id
+    INNER JOIN (
+    SELECT id, MAX(transactiondate) as last_trans_date 
+    FROM predictions_2017
+    GROUP BY id
+        ) predictions ON predictions.id = properties_2017.id AND predictions_2017.transactiondate             predictions.last_trans_date
+    LEFT OUTER JOIN propertylandusetype
+    USING(propertylandusetypeid)
+      LEFT OUTER JOIN storytype
+    USING(storytypeid)
+    LEFT OUTER JOIN typeconstructiontype
+    USING(typeconstructiontypeid)
+    JOIN unique_properties
+      ON unique_properties.parcelid = properties_2017.parcelid
+    WHERE latitude IS NOT NULL and longitude IS NOT NULL;
+    
+    ''', get_connection("zillow"))
+    return df
